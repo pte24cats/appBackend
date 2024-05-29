2035af7f50753a09dbff7fcfb9d7e0c149feb50a
darkGirl12 <cocopromise123@gmail.com>
2024-05-29 22:49:22 +0200

    .

--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+//Login route
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+//Register route
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
