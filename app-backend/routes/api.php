cea34c2ac97ee083070fbbd6a8bf5dfab59f708e
Alaa Dardari <al.dard101@gmail.com>
2024-06-02 10:38:07 +0200

    Added functionalities for browsing and choosing cats.
    (+) Added Cat controller.
    (=) Modified Routes to account for cat controller.

--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatController;
 
 //Login route
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
@@ -11,6 +12,8 @@ Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 //Register route
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
+Route::get('/cats', [CatController::class, 'index']);
+Route::get('/cats/{cat}/choose', [CatController::class, 'choose']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
