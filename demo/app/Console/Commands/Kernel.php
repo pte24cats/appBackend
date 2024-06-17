b8aa04edde7ac0e5e14bb5ed6daecd2a68266d8c
darkGirl12 <cocopromise123@gmail.com>
2024-06-17 16:56:53 +0200

    login and register changes backend.

---
+++ demo/app/Console/Commands/Kernel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // Define your scheduled commands here
+        // Example:
+        // $schedule->command('inspire')->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands'); // Adjust the path to App\Console\Commands
+
+        // Register additional commands defined in routes/console.php
+        require base_path('routes/console.php');
+    }
+}
